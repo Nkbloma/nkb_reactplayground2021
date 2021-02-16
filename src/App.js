dd3d183dd6d3d73bc56c31b76f8365c38fd52657
Nkbloma <nkbloma@gmail.com>
2021-02-16 15:05:33 -0800

    ChangedBranchToTestApi

--- src/App.js
+++ src/App.js
@@ -2,38 +2,19 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Header from './components/layout/Header'
 import './App.css';
-import Todos from './components/Todos';
-import AddTodo from './components/AddTodo';
-import About from './components/pages/About';
 import axios from 'axios';
+import HelloForm from './components/HelloForm';
 
 class App extends Component{
   state = {
-    todos: []
+    ReturnedMessage: []
   };
-
+  bloopData = "";
   componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
-    .then(res => this.setState({todos: res.data}));
+    axios.get('https://s3c1zl14xi.execute-api.us-east-2.amazonaws.com/test/helloworld?')
+    .then(res => console.log(res.data))
   }
   //Toggles Completed
-  markComplete = (id) =>{
-    this.setState({
-        todos: this.state.todos.map(todo => {
-          if(todo.id === id){
-            todo.completed=!todo.completed
-          }
-          return todo;
-        })
-      });
-  }
-
-  delTodo = (id) =>{
-    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-    .then(
-      res=>  this.setState({ todos: [...this.state.todos.filter(todo => todo.id !==id)]})
-    );
-  }
 
   addTodo = (title) =>{
     axios.post('https://jsonplaceholder.typicode.com/todos', {
@@ -45,22 +26,11 @@ class App extends Component{
   }
   render(){
     return (
-      <Router>
-        <div className="App">
-          <div className="container">
-            <Header />
-            <Route exact path="/" render={props=>(
-                <React.Fragment>
-                    <AddTodo addTodo={this.addTodo}/>
-                    <Todos todos={this.state.todos} markComplete=
-                    {this.markComplete} delTodo={this.delTodo}/>
-                </React.Fragment>
-            )}/>
-            <Route path="/about" component={About}/>
-          </div>
-        </div>
-      </Router>
-    );
+      <div>
+        <HelloForm/>
+        <p>{this.state.ReturnedMessage}</p>
+      </div>
+    )
   }
 }
 export default App;
---
+++ src/components/HelloForm.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import PropTypes from 'prop-types';
+
+export class HelloForm extends Component{
+    render(){
+        return(
+            <form>
+                <h1>sdfsdfd</h1>
+            </form>
+        )
+    }
+}
+export default HelloForm
